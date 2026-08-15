e522eb3fa1e5c00a6be026648c21ef8f532a316b
EFU CMS <cms@efu-content.hu>
2026-08-15 14:50:00 +0000

    Fix duplicate images property in open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,22 +2,6 @@
 // https://opennext.js.org/cloudflare
 
 export default {
-  // Minimal config - uses sensible defaults
-  images: {
-    domains: ['imagedelivery.net', 'cloudflarestream.com'],
-  },
-
-  // Minimal config - uses sensible defaults
-  images: {
-    domains: ['imagedelivery.net', 'cloudflarestream.com'],
-  },
-
-  // Minimal config - uses sensible defaults
-  images: {
-    domains: ['imagedelivery.net', 'cloudflarestream.com'],
-  },
-
-  // Minimal config - uses sensible defaults
   images: {
     domains: ['imagedelivery.net', 'cloudflarestream.com'],
   },
