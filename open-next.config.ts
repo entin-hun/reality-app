743ef8a81acb21e8a8649effbf0b3d4bd51f36bd
EFU CMS <cms@efu-content.hu>
2026-08-15 14:20:54 +0000

    Upgrade to Next.js 15 + Cloudflare Pages setup

---
+++ open-next.config.ts
@@ -0,0 +1,24 @@
+// OpenNext Cloudflare Configuration
+// https://opennext.js.org/cloudflare
+
+export default {
+  // Minimal config - uses sensible defaults
+  images: {
+    domains: ['imagedelivery.net', 'cloudflarestream.com'],
+  },
+
+  // Minimal config - uses sensible defaults
+  images: {
+    domains: ['imagedelivery.net', 'cloudflarestream.com'],
+  },
+
+  // Minimal config - uses sensible defaults
+  images: {
+    domains: ['imagedelivery.net', 'cloudflarestream.com'],
+  },
+
+  // Minimal config - uses sensible defaults
+  images: {
+    domains: ['imagedelivery.net', 'cloudflarestream.com'],
+  },
+};
